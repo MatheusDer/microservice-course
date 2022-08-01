75316a3ffa68b41654fb10dfe622bead40890476
Matheus Der <matheusder1@hotmail.com>
2022-08-01 18:00:23 -0300

    feat(platformservice): add platform dtos and profile

--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -10,6 +10,8 @@ builder.Services.AddDbContext<AppDbContext>(o => o.UseInMemoryDatabase("InMem"))
 
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
