32c8aca69ba29ccc8eeafa89f8a3dfa213a32714
Matheus Der <matheusder1@hotmail.com>
2022-08-11 16:22:22 -0300

    feat(commandservice): create get platforms, get command, get and create commands enpoints

--- CommandService/Program.cs
+++ CommandService/Program.cs
@@ -1,4 +1,7 @@
+using AutoMapper;
 using CommandService.Data;
+using CommandService.Dtos;
+using CommandService.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +13,6 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<ICommandRepo, CommandRepo>();
 
-
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -20,15 +22,70 @@ if (app.Environment.IsDevelopment())
 }
 
 #region PLATFORM
+
+app.MapGet("api/v1/c/platforms", (ICommandRepo repo, IMapper mapper) =>
+{
+    Console.WriteLine("--> Getting platforms from CommandService");
+
+    var platforms = repo.GetAllPlatforms();
+
+    return Results.Ok(mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
+});
+
 app.MapPost("api/v1/c/platforms", () =>
 {
     Console.WriteLine("--> Inbound POST # Command Service");
 
     return Results.Ok("Inbound test from Platforms Endpoint");
 });
+
 #endregion
 
 #region COMMAND
+
+app.MapGet("api/v1/c/platforms/{platformId}/commands", (ICommandRepo repo, IMapper mapper, int platformId) =>
+{
+    Console.WriteLine("--> Hit GET platforms/{platformId}/commands: " + platformId);
+
+    if (!repo.PlatformExists(platformId))
+        return Results.NotFound();
+
+    var commands = repo.GetByPlatformId(platformId);
+
+    return Results.Ok(mapper.Map<IEnumerable<CommandReadDto>>(commands));
+});
+
+app.MapGet("api/v1/c/platforms/{platformId}/commands/{commandId}", (ICommandRepo repo, IMapper mapper, int platformId, int commandId) =>
+{
+    Console.WriteLine("--> Hit GET platforms/{platformId}/commands/{commandId}: " + platformId + " " + commandId);
+
+    if (!repo.PlatformExists(platformId))
+        return Results.NotFound();
+
+    var command = repo.GetById(platformId, commandId);
+    if (command is null)
+        return Results.NotFound();
+
+    return Results.Ok(mapper.Map<CommandReadDto>(command));
+});
+
+app.MapPost("api/v1/c/platforms/{platformId}/commands", (ICommandRepo repo, IMapper mapper, CommandCreateDto createDto, int platformId) =>
+{
+    Console.WriteLine("--> Hit POST platforms/{platformId}/commands: " + platformId);
+
+    if (!repo.PlatformExists(platformId))
+        return Results.NotFound();
+
+    var command = mapper.Map<Command>(createDto);
+
+    repo.Create(platformId, command);
+    repo.SaveChanges();
+
+    var commandRead = mapper.Map<CommandReadDto>(command);
+
+    return Results.Created($"api/v1/c/platforms/{platformId}/{commandRead.Id}", commandRead);
+});
+
 #endregion
 
 //app.UseHttpsRedirection();
